4f9dd92311b11adb74ffd60a4911d48787d0d76d
Alexander Pensavalle <alexanderpensavalle@Alexanders-MacBook-Pro.local>
2020-10-16 15:46:37 -0400

    Updates

---
+++ precache-manifest.8191ae760c6bbaa4f488e97403c45d09.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba6305553b15484da33fae05d296c9ca",
+    "url": "/alexpensavalle/project-4-assessment-alex-p-circle-selector.git/index.html"
+  },
+  {
+    "revision": "7f414f8cddeebcbbe667",
+    "url": "/alexpensavalle/project-4-assessment-alex-p-circle-selector.git/static/css/main.2ddb3bd6.chunk.css"
+  },
+  {
+    "revision": "ae5ce2c1012788efe90c",
+    "url": "/alexpensavalle/project-4-assessment-alex-p-circle-selector.git/static/js/2.e8fb8908.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/alexpensavalle/project-4-assessment-alex-p-circle-selector.git/static/js/2.e8fb8908.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f414f8cddeebcbbe667",
+    "url": "/alexpensavalle/project-4-assessment-alex-p-circle-selector.git/static/js/main.468ef28c.chunk.js"
+  },
+  {
+    "revision": "5a5d9bbd4d9079dec370",
+    "url": "/alexpensavalle/project-4-assessment-alex-p-circle-selector.git/static/js/runtime-main.26b853bb.js"
+  }
+]);
